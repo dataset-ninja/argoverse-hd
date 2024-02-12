d4a51fdb81a3a668cb7640433a5809477c3b8f30
peixed351 <peixed351@gmail.com>
2024-02-12 22:07:21 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -37,7 +37,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://mtli.github.io/streaming/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 13766218
+PREVIEW_IMAGE_ID: int = 13824359
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/argoverse-hd"
